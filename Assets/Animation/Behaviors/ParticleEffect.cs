cd3e3931bd47d782786cc67c4bef131741effba1
Derek Stobbe <djstobbe@gmail.com>
2016-03-20 18:02:35 -0600

    Add particle effect burst on landing

---
+++ Assets/Animation/Behaviors/ParticleEffect.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticleEffect : StateMachineBehaviour
+{
+	[SerializeField]
+	protected GameObject effectPrefab;
+	[SerializeField]
+	protected float minVelocity;
+
+	private Transform effectTransform;
+	private ParticleSystem effectInstance;
+
+	public override void OnStateEnter (Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+	{
+		if (effectInstance == null) {
+			effectTransform = GameObject.Instantiate (effectPrefab).transform;
+			effectInstance = effectTransform.gameObject.GetComponentInChildren<ParticleSystem> ();
+		}
+
+		Debug.Log (animator.GetFloat ("vVelocity"));
+		if (animator.GetFloat ("vVelocity") <= minVelocity) {
+			effectTransform.position = animator.transform.position;
+			effectInstance.Play (true);
+		}
+	}
+}
--- Assets/Scripts/CharacterController2D.cs
+++ Assets/Scripts/CharacterController2D.cs
@@ -32,11 +32,13 @@ public class CharacterController2D : MonoBehaviour
 
 	protected Rigidbody2D body;
 	new protected Collider2D collider;
+	protected Animator animator;
 
 	protected void Awake ()
 	{
 		body = GetComponent<Rigidbody2D> ();
 		collider = GetComponent<Collider2D> ();
+		animator = GetComponent<Animator> ();
 	}
 
 	protected void Update ()
@@ -53,6 +55,14 @@ public class CharacterController2D : MonoBehaviour
 
 			if (IsGrounded) {
 				body.AddForce (Vector2.up * jumpForce, ForceMode2D.Impulse);
+				animator.SetTrigger ("didJump");
+				animator.SetBool ("isJumping", true);
+			}
+
+			if (IsWallGrabbing) {
+				Vector2 jumpDir = IsTouching (CollisionDirections.Left) ? Vector2.one : new Vector2 (-1, 1);
+				Debug.DrawRay (body.position, body.position + jumpDir * jumpForce);
+				body.AddForce (jumpDir * jumpForce, ForceMode2D.Impulse);
 			}
 		}
 
@@ -69,6 +79,9 @@ public class CharacterController2D : MonoBehaviour
 		}
 
 		body.velocity = velocity;
+
+		animator.SetFloat ("hVelocity", velocity.x);
+		animator.SetFloat ("vVelocity", velocity.y);
 	}
 
 	protected void OnCollisionEnter2D (Collision2D coll)
@@ -84,6 +97,8 @@ public class CharacterController2D : MonoBehaviour
 				collisions |= CollisionDirections.Up;
 			} else if (cp.normal == Vector2.up) {
 				collisions |= CollisionDirections.Down;
+				animator.SetTrigger ("didLand");
+				animator.SetBool ("isJumping", false);
 			} else if (cp.normal == Vector2.right) {
 				collisions |= CollisionDirections.Left;
 				wallTouchAt = Time.time;
@@ -119,6 +134,13 @@ public class CharacterController2D : MonoBehaviour
 	public bool IsGrounded {
 		get {
 			return IsTouching (CollisionDirections.Down);
+		}
+	}
+
+	public bool IsWallGrabbing {
+		get {
+			CollisionDirections flags = CollisionDirections.Left | CollisionDirections.Right;
+			return (collisions & flags) == CollisionDirections.Left || (collisions & flags) == CollisionDirections.Right;
 		}
 	}
 
